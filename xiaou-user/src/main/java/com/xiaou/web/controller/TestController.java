2b70b4f67c508c8e9eaa8109bedf91e309b9e44f
aliyun1153623907 <3153566913@qq.com>
2025-04-14 19:44:43 +0800

    feat: redis封装

--- xiaou-user/src/main/java/com/xiaou/web/controller/TestController.java
+++ xiaou-user/src/main/java/com/xiaou/web/controller/TestController.java
@@ -1,12 +1,25 @@
 package com.xiaou.web.controller;
 
+import com.xiaou.redis.utils.RedisUtils;
+import jakarta.annotation.Resource;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Resource
+    private RedisUtils redisUtils;
+
     @GetMapping
-    public String test(){
-      return "欢迎来到企业级开发框架";
+    public String test() {
+        return "欢迎来到企业级开发框架";
     }
+
+    @GetMapping("/test/redis")
+    public void testRedis() {
+        redisUtils.set("test", "test");
+    }
+
 }
