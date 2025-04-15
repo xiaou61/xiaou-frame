6f2ace72368dab3d052c175308f37a099473f79b
aliyun1153623907 <3153566913@qq.com>
2025-04-15 14:48:10 +0800

    feat: 集成log4j2

--- xiaou-user/src/main/java/com/xiaou/UserApplication.java
+++ xiaou-user/src/main/java/com/xiaou/UserApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableCaching
 public class UserApplication {
 
 
